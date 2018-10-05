dc748f2eeb00f56c4bcbd1319047f4bb336821dd
tiagojard <tiago.jardcarvalho@gmail.com>
2018-10-05 19:50:06 -0300

    sdfsfd

--- src/App.js
+++ src/App.js
@@ -37,15 +37,21 @@ class App extends Component {
       <div>
         <div>
           <header className="App-header">
-          <a href="/" title="Home">
-          <img src={logo} alt="Logo" className="App-logo"/>
-          </a>
-           
-            <div className="busca">
-              <input type="text" id="busca" placeholder="Pesquisar" className="input-busca" />
-              <input id="id" type="hidden" />
-              
-            </div>
+           <div className="bloco-logo">
+              <a href="/" title="Home">
+                <img src={sitemap} alt="Logo" className="App-logo"/>
+              </a>
+           </div>
+              <div>
+                <div className="busca">
+                  <input type="text" id="busca" placeholder="Pesquisar" className="input-busca" />
+                  <input id="id" type="hidden" />
+                  </div>  
+                  <svg fill="#fff" class="search-icon" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+        <path d="M0 0h24v24H0z" fill="none"></path>
+    </svg> 
+              </div>
           </header>
         </div>
         
